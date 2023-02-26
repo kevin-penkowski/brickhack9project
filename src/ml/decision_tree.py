aa9393cd14e4ab3ba0a6b64bbdbb0fdd76576e72
Tianna Seitz <tjs1980@rit.edu>
2023-02-26 08:25:50 -0500

    experimented with different amoutns of test and train samples to prevent overfitting

--- src/ml/decision_tree.py
+++ src/ml/decision_tree.py
@@ -4,7 +4,8 @@ from sklearn.tree import DecisionTreeClassifier
 def run(x_train, y_train, x_test, y_test, clf):
     train(x_train, y_train, clf) #this is the training step
     print(" predictions: ", predict(x_test, clf)) #this tries out the model on the held out dataset
-    print(" actual labels: ", y_test) #this vector is our actual labeld
+    print(" actual labels: ", y_test) #this vector is our actual labels
+    print(len(y_test))
     print("score = %0.4f" % clf.score(x_test, y_test)) #this compares the predicted to actual classifier and calculates accuracy. Score = (num of test samples correct)/(num of test correct + num of test wrong)
     print()
 
@@ -31,8 +32,11 @@ def get_training_set():
 if __name__ == "__main__":
     features = np.load("./src/ml/mushroom_features.npy")
     labels = np.load("./src/ml/mushroom_labels.npy")
+    
+    # N = 300
+    N = 400
 
-    N = 300
+    #All experiments have resulted in a score of 1.00 (experiments: 300 train samples with 20 test samples and then 200 test samples; 400 train samples with 100 test samples)
 
     features_train = features[:N]; features_test = features[N:]
     labels_train = labels[:N]; labels_test = labels[N:]
