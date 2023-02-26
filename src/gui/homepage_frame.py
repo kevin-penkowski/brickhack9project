7589766cf6cd4808027fac4408d510cf03f2b3ce
Kevin Penkowski <kwp5892@rit.edu>
2023-02-26 08:17:11 -0500

    SNIP SNIP

--- src/gui/homepage_frame.py
+++ src/gui/homepage_frame.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import gui.main_window as main_window
+from PIL import Image, ImageTk
 
 class HomepageFrame():
     def __init__(self, parent, quiz_transition_func):
@@ -10,6 +11,10 @@ class HomepageFrame():
 
         self.title = tk.Label(self.frame, text=main_window.PROJECT_TITLE, font=("Arial", 56, "bold"))
         self.title.pack(side=tk.TOP)
+
+        self.diagram = ImageTk.PhotoImage(Image.open("./src/gui_images/mushroom_diagram.jpg").resize((120,200), Image.BICUBIC))
+        self.diagram_label = tk.Label(self.frame, image=self.diagram)
+        self.diagram_label.pack()
 
         self.button = tk.Button(self.frame, text="Take our quiz!", command=quiz_transition_func)
         self.button.pack(side=tk.BOTTOM)
